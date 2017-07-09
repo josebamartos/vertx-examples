f9dd1cef977f51ee865a0cb5598a0d1a47e37747
Joseba <joseba@otzarri.net>
2017-07-08 22:46:42 -0500

    Added minimal-project example

---
+++ minimal-project/src/main/java/net/otzarri/examples/vertx/minimal_project/MainVerticle.java
@@ -0,0 +1,15 @@
+package net.otzarri.examples.vertx.minimal_project;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+	
+	@Override
+	public void start() throws Exception {
+		vertx.createHttpServer()
+		.requestHandler(req -> {
+			req.response().end("Hello from " + Thread.currentThread().getName());
+		})
+		.listen(8080);
+		}
+	}
