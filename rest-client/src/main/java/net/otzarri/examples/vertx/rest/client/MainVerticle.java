0c60257b5ab9b07d80be6244ce2255de4ff0f14b
Joseba <joseba@otzarri.net>
2017-07-09 06:14:42 -0500

    Added rest-client example project

---
+++ rest-client/src/main/java/net/otzarri/examples/vertx/rest/client/MainVerticle.java
@@ -0,0 +1,39 @@
+package net.otzarri.examples.vertx.rest.client;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.client.HttpRequest;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
+
+public class MainVerticle extends AbstractVerticle {
+
+	@Override
+	public void start() {
+		Router router = Router.router(vertx);
+		
+		router.get("/").handler(this::invokeGreet);
+		
+		vertx.createHttpServer()
+		.requestHandler(router::accept)
+		.listen(8081);
+	}
+	
+	private void invokeGreet(RoutingContext rc) {
+		WebClient client = WebClient.create(vertx);
+		
+		HttpRequest<JsonObject> request = client
+		.get(8080, "localhost","/Vert.x client")
+		.as(BodyCodec.jsonObject());
+
+		request.send(ar -> {
+			if (ar.failed()) {
+				rc.fail(ar.cause());
+			} else {
+				rc.response().end(ar.result().body().encode());
+			}
+		});
+	}
+}
